e6fe71fad3a2510cd23e5ddcca3b919a2db33cd3
Deepika-Garg <garg.deepika21@gmail.com>
2025-04-16 17:12:35 +0100

    pallindrome project done

---
+++ Pallindrome/script.js
@@ -0,0 +1,24 @@
+
+const input=document.getElementById("input");
+
+function reverse(str){
+
+}
+function check()
+{
+   const value=input.value;
+
+let j=value.length;
+for(let i=0;i<=i/2;i++)
+{
+    if(value[i]!==value[j-1])
+    {
+        alert("not today!")
+    return;
+    }
+    else
+        j--;
+}
+alert("pallindrome");
+
+}
